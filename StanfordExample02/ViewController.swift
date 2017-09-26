b2587e40bc6a0350727b6593d98def31053438ad
Minki <cmk330@gmail.com>
2017-09-26 12:02:29 +0900

    Initial Commit

---
+++ StanfordExample02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StanfordExample02
+//
+//  Created by Minki on 2017. 9. 26..
+//  Copyright © 2017년 Minki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
